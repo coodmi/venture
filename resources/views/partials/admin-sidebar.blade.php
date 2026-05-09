40c0dcb8d754fdb1b9915c3f2f31d42e57b3a461
Asif Mollik <115562335+asifmollik12@users.noreply.github.com>
2026-05-09 20:25:12 +0600

    Admin sidebar: Startups now links to opportunities list, not seeker profiles

--- resources/views/partials/admin-sidebar.blade.php
+++ resources/views/partials/admin-sidebar.blade.php
@@ -58,7 +58,7 @@
         @php
         $peopleItems = [
             ['route'=>'admin.investors.index',        'label'=>'Investors',  'color'=>'#1a3c8f', 'activeBg'=>'#eff6ff', 'icon'=>'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z'],
-            ['route'=>'admin.startups-profiles.index','label'=>'Startups',   'color'=>'#f97316', 'activeBg'=>'#fff7ed', 'icon'=>'M13 10V3L4 14h7v7l9-11h-7z'],
+            ['route'=>'admin.opportunities.index',    'label'=>'Startups',   'color'=>'#f97316', 'activeBg'=>'#fff7ed', 'icon'=>'M13 10V3L4 14h7v7l9-11h-7z'],
         ];
         @endphp
 
